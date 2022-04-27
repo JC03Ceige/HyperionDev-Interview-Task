d5aca9a22de48e6344c7c0829796bf1bd55dc688
JCvdMerwe <jcvdmerwe03@gmail.com>
2022-04-27 16:14:52 +0200

    Original Working Base

--- SectC_Opt4/isbn_validator.js
+++ SectC_Opt4/isbn_validator.js
@@ -1,28 +1,25 @@
 let isbn = "";
 let valid = false;
 
-let isbnValidator = (isbn) => {
+isbnValidator = (isbn) => {
   // We convert the ISBN into an Array for easier iteration and manipulation.
   let isbnArr = isbn.split("").map((num) => {
     return parseInt(num, 10);
   });
+
   console.log(isbnArr);
 
-  // We convert the digit array based on isbn check standards.
-  let digitConvert = () => {
-    // We check to see if it is a 10 digit ISBN
-    if (isbnArr.length === 10) {
-      let multiplier = isbnArr.length;
-      let tempArr = [];
-      for (let i = 0; i < isbnArr.length; i++) {
-        let numCheck = isbnArr[i] * multiplier;
-        tempArr.push(numCheck);
-        multiplier--;
-        console.log(multiplier);
-        console.log(tempArr);
-      }
+  // We check to see if it is a 10 digit ISBN
+  if (isbnArr.length === 10) {
+    let multiplier = 10;
+    let tempArr = [];
+    for (let i = 0; i < isbnArr.length; i++) {
+      let numCheck = isbnArr[i] * multiplier;
+      tempArr.push(numCheck);
+      multiplier--;
+      console.log(multiplier);
+      console.log(tempArr);
     }
-
     let arrSum = tempArr.reduce((total, currentValue) => total + currentValue);
     console.log(arrSum);
 
@@ -30,34 +27,19 @@ let isbnValidator = (isbn) => {
       if (arrSum % 11 === 0) {
         valid = true;
         console.log(valid);
-        //return valid;
+        return valid;
       } else {
-        isbnArr[9]++;
-        console.log(isbnArr);
-        let tempArr = [];
-        for (let i = 0; i < isbnArr.length; i++) {
-          let multiplier = isbnArr.length;
-
-          let numCheck = isbnArr[i] * multiplier;
-          tempArr.push(numCheck);
-          multiplier--;
-          console.log(multiplier);
-          console.log(tempArr);
-        }
+        tempArr[9]++;
         arrSum = tempArr.reduce((total, currentValue) => total + currentValue);
       }
       console.log(tempArr);
     }
     tempArr.unshift(978);
-    console.log(tempArr);
 
     isbn = tempArr.toString();
     console.log(isbn);
-    // Removing all the commas
-    var result = isbn.replace(/,/g, "");
-    console.log(result);
-  };
-  return result;
+  }
+  return isbn;
 };
 
 isbnValidator("8624593252");
